75226732f54592759c88fe4c8a5d49c149a21003
Xingchen Chen <92684741+esemsc-xc1924@users.noreply.github.com>
2025-03-01 14:24:50 +0000

    test openai api

--- test_openai_api.py
+++ test_openai_api.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  # api_key="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"
 )
 
 completion = client.chat.completions.create(
